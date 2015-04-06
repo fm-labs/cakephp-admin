94e2f2fefddb9377fa08ad82ab289cb80cd51cdb
fm-labs <flowmotionlabs@gmail.com>
2015-04-06 21:15:28 +0200

    updated layout elements and added default config

---
+++ config/backend.default.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'Backend' => [
+        'title' => 'Dashboard',
+        'dashboardUrl' => '/backend/admin/Backend/default',
+        'loginUrl' => '/users/login',
+        'logoutUrl' => '/users/logout'
+    ]
+];

235a1787fce27f9a25098e2db20734a45f6ebe26
fm-labs <flowmotionlabs@gmail.com>
2016-02-16 18:30:47 +0100

    added Security and Auth backend config scopes

--- config/backend.default.php
+++ config/backend.default.php
@@ -1,12 +1,37 @@
 <?php
 return [
-    # General backend settings
+    /**
+     * Backend Dashboard
+     *
+     * - title: Dashboard title string
+     * - url: Url to Dashboard
+     */
     'Backend.Dashboard' => [
         'title' => 'Backend',
         'url' => '/backend/admin/Default/index',
     ],
-//    # Backend users for basic auth
-//    'Backend.Users' => [
-//          'admin' => 'myAdminPa$$w0rd'
-//    ],
+
+    /**
+     * Backend Security
+     *
+     * - enabled: Enables SecurityComponent
+     * - forceSSL: Force https scheme for all backend requests
+     */
+    'Backend.Security' => [
+        'enabled' => false,
+        'forceSSL' => false
+    ],
+
+    /**
+     * Backend AuthComponent config
+     */
+    'Backend.Auth' => [
+    ],
+
+    /**
+     * Backend Basic Auth Users
+     */
+    'Backend.Users' => [
+        //'admin' => 'myAdminPa$$w0rd'
+    ],
 ];

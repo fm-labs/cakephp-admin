77f47eafaf42f5a45c19f37667a6906b2832fce9
fm-labs <flowmotionlabs@gmail.com>
2016-02-16 18:30:18 +0100

    removed deprecated media routes

--- config/routes.php
+++ config/routes.php
@@ -6,11 +6,6 @@ Router::plugin('Backend', function ($routes) {
 
         $routes->extensions(['json']);
 
-        $routes->connect('/media/', ['controller' => 'MediaBrowser', 'action' => 'tree', 'config' => 'default']);
-        $routes->connect('/media/:config/', ['controller' => 'MediaBrowser', 'action' => 'tree']);
-        $routes->connect('/media/:config/:action', ['controller' => 'MediaBrowser']);
-
-
         $routes->connect('/:controller');
         $routes->fallbacks('DashedRoute');
     });

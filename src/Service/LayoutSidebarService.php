9b4e9033407ae8ecb69949ffdafd3477c4c55ac1
fm-labs <flowmotionlabs@gmail.com>
2017-11-22 16:10:23 +0100

    inject sidebar menu from LayoutSidebarService; dropped BackendMenuCell

--- src/Service/LayoutSidebarService.php
+++ src/Service/LayoutSidebarService.php
@@ -4,11 +4,16 @@ namespace Backend\Service;
 
 use Backend\BackendService;
 use Backend\View\BackendView;
+use Banana\Menu\Menu;
 use Cake\Core\Configure;
 use Cake\Event\Event;
+use Cake\Event\EventDispatcherInterface;
+use Cake\Event\EventDispatcherTrait;
 
-class LayoutSidebarService extends BackendService
+class LayoutSidebarService extends BackendService implements EventDispatcherInterface
 {
+    use EventDispatcherTrait;
+
     public function implementedEvents()
     {
         return [
@@ -22,9 +27,19 @@ class LayoutSidebarService extends BackendService
     public function beforeLayout(Event $event)
     {
         if ($event->subject() instanceof BackendView) {
+
+            // inject sidebar menu
+            $menu = new Menu();
+            $_event = new Event('Backend.Menu.get', $menu);
+            $this->eventManager()->dispatch($_event);
+            $event->subject()->set('backend.sidebar.menu', $menu);
+
+            // inject sidebar elements
             $sidebar = [
                 'backend_sidebar' => [
                     'menu' => ['element' => 'Backend.Layout/admin/sidebar/sidebar_menu'],
+                    //'search' => ['element' => 'Backend.Layout/admin/sidebar/sidebar_search'],
+                    //'user' => ['element' => 'Backend.Layout/admin/sidebar/sidebar_user'],
                 ],
             ];
 

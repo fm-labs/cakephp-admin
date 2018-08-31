441c0750ca18c185ccc7c36f5ff360fadb40d6ab
fm-labs <flowmotionlabs@gmail.com>
2018-08-31 13:50:26 +0200

    added BackendController

---
+++ src/Controller/Admin/BackendController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Backend\Controller\Admin;
+
+use Cake\Core\Configure;
+
+/**
+ * Class BackendController
+ *
+ * Default Dashboard controller
+ *
+ * @package Backend\Controller\Admin
+ */
+class BackendController extends AppController
+{
+    public $actions = [
+        'index' => 'Backend.Dashboard'
+    ];
+
+    /**
+     * Default Dashboard
+     */
+    public function index()
+    {
+        $this->set('dashboard.title', __d('backend','System Dashboard'));
+        $this->set('dashboard.panels', Configure::read('Backend.Dashboard.Panels'));
+        $this->Action->execute();
+    }
+
+    /**
+     * Backend Dashboard
+     * @deprecated
+     */
+    public function backend()
+    {
+        $this->Flash->warning('Dashboard::backend is deprecated');
+    }
+}

a2f15f5493404aa49fae9dc292043f963c431618
fm-labs <flowmotionlabs@gmail.com>
2019-03-08 18:43:52 +0100

    added VectorMapHelper using jqvmap.js

---
+++ src/View/Helper/VectorMapHelper.php
@@ -0,0 +1,43 @@
+<?php
+namespace Backend\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * VectorMap helper.
+ *
+ * Render vector maps using jqvmap.js
+ * @link https://www.10bestdesign.com/jqvmap/documentation/
+ *
+ * @property \Cake\View\Helper\HtmlHelper $Html
+ */
+class VectorMapHelper extends Helper
+{
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'scriptUrl' => 'Backend./libs/jqvmap/jquery.vmap.min.js',
+        'cssUrl' => 'Backend./libs/jqvmap/jqvmap.min.css',
+        'mapsBaseUrl' => 'Backend./libs/jqvmap/maps/'
+    ];
+
+    public $helpers = ['Html'];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize(array $config)
+    {
+        $this->Html->css($this->config('cssUrl'), ['block' => true]);
+        $this->Html->script($this->config('scriptUrl'), ['block' => true]);
+    }
+
+    public function create()
+    {
+
+    }
+}

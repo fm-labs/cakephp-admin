f9febf1d9a1c6789a733ff28175c941693e707cb
fm-labs <flowmotionlabs@gmail.com>
2019-03-08 18:43:27 +0100

    added FlagIconHelper

---
+++ tests/TestCase/View/Helper/FlagIconHelperTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Backend\Test\TestCase\View\Helper;
+
+use Backend\View\Helper\FlagIconHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+/**
+ * Backend\View\Helper\FlagIconHelper Test Case
+ */
+class FlagIconHelperTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \Backend\View\Helper\FlagIconHelper
+     */
+    public $FlagIcon;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $view = new View();
+        $this->FlagIcon = new FlagIconHelper($view);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->FlagIcon);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}

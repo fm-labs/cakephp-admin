7ec33be46d0b00696a639b9a42b5f5b8d2fbdb64
fm-labs <flowmotionlabs@gmail.com>
2023-02-02 01:18:48 +0100

    close sidebar button

--- templates/element/layout/admin/sidebar_sticky.php
+++ templates/element/layout/admin/sidebar_sticky.php
@@ -1,3 +1,17 @@
 <div class="position-sticky pt-3 sidebar-sticky">
     <?= $this->fetch('sidebar_content') ?>
+
+    <div id="sidebar-close" class="p-3">
+        <a href="#" id="sidebar-close-btn">Close</a>
+    </div>
+    <?php $this->append('script'); ?>
+    <script>
+        $('#sidebar-close-btn').click((e) => {
+            console.log("clicked");
+            $('#sidebarMenu').hide().remove();
+            e.stopPropagation();
+            e.preventDefault();
+        })
+    </script>
+    <?php $this->end(); ?>
 </div>

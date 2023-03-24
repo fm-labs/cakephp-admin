87865911fb09c03513cac18cf0be05475ad7c1d4
fm-labs <flowmotionlabs@gmail.com>
2023-03-24 12:39:32 +0100

    Added global view var 'toolbar_noheading', disabled heading rendering in toolbar.

--- templates/element/layout/admin/toolbar.php
+++ templates/element/layout/admin/toolbar.php
@@ -2,22 +2,27 @@
 /**
  * @var \Cake\View\View $this
  */
-$this->loadHelper('Admin.Toolbar');
-
-$title = $this->fetch('title');
-$heading = $this->fetch('heading');
 $toolbarDisabled = $this->get('toolbar_disabled', false);
-
 if ($toolbarDisabled) {
     return false;
 }
+
+// @todo Move automatic title assignment to helper or admin view
+$title = $this->fetch('title');
+
+$heading = $this->fetch('heading', $title);
+if ($this->get('toolbar_noheading')) {
+    $heading = false;
+}
+
+$this->loadHelper('Admin.Toolbar');
 ?>
 <div class="main-toolbar">
     <div class="container-fluid">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-1 mb-3 border-bottom toolbar">
             <div>
-                <?php if ($heading !== false): ?>
-                <h3><?= $heading ?? $title; ?></h3>
+                <?php if ($heading): ?>
+                <h3><?= $heading; ?></h3>
                 <?php endif; ?>
             </div>
             <div class="btn-toolbar">
